29ba42ed2ef5df0e735a8b1e35a2fd55eca51964
Martin Beránek <martin.beranek@embedit.cz>
2019-06-21 07:25:09 +0200

    Enable configuration of http proxy

---
+++ files/scripts/system/http.groovy
@@ -0,0 +1,37 @@
+import groovy.json.JsonSlurper
+
+parsed_args = new JsonSlurper().parseText(args)
+
+core.removeHTTPProxy()
+if (parsed_args.http_proxy_host) {
+    if (parsed_args.http_proxy_username) {
+        core.httpProxyWithBasicAuth(
+                parsed_args.http_proxy_host,
+                parsed_args.http_proxy_port as int,
+                parsed_args.http_proxy_username,
+                parsed_args.http_proxy_password)
+    } else {
+        core.httpProxy(
+                parsed_args.http_proxy_host,
+                parsed_args.http_proxy_port as int)
+    }
+}
+
+core.removeHTTPSProxy()
+if (parsed_args.https_proxy_host) {
+    if (parsed_args.https_proxy_username) {
+        core.httpsProxyWithBasicAuth(
+                parsed_args.https_proxy_host,
+                parsed_args.https_proxy_port as int,
+                parsed_args.https_proxy_username,
+                parsed_args.https_proxy_password)
+    } else {
+        core.httpsProxy(
+                parsed_args.https_proxy_host,
+                parsed_args.https_proxy_port as int)
+    }
+}
+
+if (parsed_args.http_proxy_host || parsed_args.https_proxy_host) {
+    core.nonProxyHosts(parsed_args.no_proxy.split(','))
+}
