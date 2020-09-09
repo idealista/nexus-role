037d6293e998e701e4de91575dbe568c7b80aaf2
Victor Suarez Fernandez <vsuarez@idealista.com>
2020-09-09 15:48:17 +0200

    First composer support

---
+++ files/scripts/composer/create_hosted_repository.groovy
@@ -0,0 +1,35 @@
+import groovy.json.JsonSlurper
+import org.sonatype.nexus.repository.config.Configuration
+
+parsed_args = new JsonSlurper().parseText(args)
+
+configuration = new Configuration(
+        repositoryName: parsed_args.name,
+        recipeName: 'composer-hosted',
+        online: true,
+        attributes: [
+                composer: [
+                        httpPort: parsed_args.http_port,
+                        v1Enabled : parsed_args.v1_enabled
+                ],
+                storage: [
+                        writePolicy: parsed_args.write_policy.toUpperCase(),
+                        blobStoreName: parsed_args.blob_store,
+                        strictContentTypeValidation: Boolean.valueOf(parsed_args.strict_content_validation)
+                ],
+                cleanup: [
+                        policyName: parsed_args.clean_policy
+                ]
+        ]
+)
+
+def existingRepository = repository.getRepositoryManager().get(parsed_args.name)
+
+if (existingRepository != null) {
+    existingRepository.stop()
+    configuration.attributes['storage']['blobStoreName'] = existingRepository.configuration.attributes['storage']['blobStoreName']
+    existingRepository.update(configuration)
+    existingRepository.start()
+} else {
+    repository.getRepositoryManager().create(configuration)
+}
