b497807d41fad87ec61803a80d72cb7f5f51939b
Victor Suarez Fernandez <vsuarez@idealista.com>
2020-09-15 12:56:12 +0200

    Adapt groovy to latest versions

--- files/scripts/composer/create_hosted_repository.groovy
+++ files/scripts/composer/create_hosted_repository.groovy
@@ -1,13 +1,16 @@
 import groovy.json.JsonSlurper
 import org.sonatype.nexus.repository.config.Configuration
+import org.sonatype.nexus.repository.manager.RepositoryManager
 
+repositoryManager = container.lookup(RepositoryManager.class.getName())
 parsed_args = new JsonSlurper().parseText(args)
 
-configuration = new Configuration(
-        repositoryName: parsed_args.name,
-        recipeName: 'composer-hosted',
-        online: true,
-        attributes: [
+Configuration configuration = repositoryManager.newConfiguration()
+configuration.with{
+        repositoryName = parsed_args.name
+        recipeName = 'composer-hosted'
+        online = true
+        attributes = [
                 composer: [
                         httpPort: parsed_args.http_port,
                         v1Enabled : parsed_args.v1_enabled
@@ -18,12 +21,12 @@ configuration = new Configuration(
                         strictContentTypeValidation: Boolean.valueOf(parsed_args.strict_content_validation)
                 ],
                 cleanup: [
-                        policyName: parsed_args.clean_policy
+                        policyName: new HashSet<String>([parsed_args.clean_policy])
                 ]
         ]
-)
+}
 
-def existingRepository = repository.getRepositoryManager().get(parsed_args.name)
+def existingRepository = repositoryManager.get(parsed_args.name)
 
 if (existingRepository != null) {
     existingRepository.stop()
@@ -31,5 +34,5 @@ if (existingRepository != null) {
     existingRepository.update(configuration)
     existingRepository.start()
 } else {
-    repository.getRepositoryManager().create(configuration)
+    repositoryManager.create(configuration)
 }
